dec9da95dda10286c49cf20702d01fe547b5778e
parasol-ka <ananastya.chau@gmail.com>
2024-12-27 13:45:02 +0100

    citations ok + css

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
     $reqSiths = $bd->prepare("SELECT * FROM `heros` WHERE secondaire=0 AND cote_obscur=1 ORDER BY premiere_apparition;");
     $reqSiths->execute();
 
-    
+    $reqName = $bd->prepare("SELECT nom from heros WHERE id_heros=:hero_id;");
+    $reqName->bindvalue(':hero_id', intval($_GET['page']));
+    $reqName->execute();
+
+    $reqCitations = $bd->prepare("SELECT h.id_heros, h.nom, f.titre, c.id_citation, c.citation FROM heros as h JOIN citations AS c on h.id_heros=c.id_heros JOIN films AS f ON c.id_film=f.id_film WHERE h.id_heros=:hero_id ORDER BY f.annee;");
+    $reqCitations->bindvalue(':hero_id', intval($_GET['page']));
+    $reqCitations->execute();
     
     session_start();
 ?>
@@ -32,7 +38,13 @@
                 $sabres = explode(';', $jedis['sabres']);
                 
 
-                echo "<a class='heros_names' href='index.php?page=" . $jedi_id . "'>" . $jedi_name;
+                echo "<a class='heros_names' href='index.php?page=" . $jedi_id ;
+                
+                if ( $_GET["page"]==$jedi_id ) {
+                    $s = "style='font-weight: bold;'";
+                } else { $s="" ;}
+
+                echo "' $s>". $jedi_name;
                 if (!empty($jedis['surnom'])){ echo " \"$jedi_nickname\"";} 
                 
 
@@ -51,7 +63,14 @@
                 $sith_nickname = $siths['surnom'];
                 $sabres = explode(';', $siths['sabres']);
 
-                echo "<a class='heros_names' href='index.php?page=" . $sith_id . "'>" . $sith_name;
+                echo "<a class='heros_names' href='index.php?page=" . $sith_id ;
+                
+                if ( $_GET["page"]==$sith_id ) {
+                    $s = "style='font-weight: bold;'";
+                } else { $s="" ;}
+
+                echo "' $s>". $sith_name;
+                if (!empty($jedis['surnom'])){ echo " \"$jedi_nickname\"";} 
                 if (!empty($siths['surnom'])){ echo " \"$sith_nickname\"";} 
                 
                 foreach ($sabres as $saber) {
@@ -64,6 +83,28 @@
     </div>
     <div id="citations">
         <?php
+        if (isset($_GET['page'])){
+            $hero_id = $_GET['page'];
+            $hero_name=$reqName->fetch();
+
+            if ($hero_name['nom'] != '') {
+                echo "<h2>Citation de " . $hero_name['nom'] . "</h2>";
+            }else {echo "<h2>Héros n'as pas de citations</h2>";}
+            
+            while ( $citations=$reqCitations->fetch() ) {
+                $citation=$citations['citation'];
+                $film=$citations['titre'];
+                echo "<p class='citation'> \"$citation\" - <span class='film_name'> $film  </span></p>";
+                
+            }
+
+        }
+
+
+        
+
+        
+
 
         ?>
     </div>
